9476de177ae4f95262d295827372d3c5682780c8
煮酒泛舟 <lixianghb@gmail.com>
2018-10-22 17:46:34 +0800

    完成

--- sc-f-e-01/eureka-client/src/main/java/com/cyinfotech/eurekaclient/EurekaClientApplication.java
+++ sc-f-e-01/eureka-client/src/main/java/com/cyinfotech/eurekaclient/EurekaClientApplication.java
@@ -1,12 +1,20 @@
 package com.cyinfotech.eurekaclient;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EurekaClientApplication {
 
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        new SpringApplicationBuilder(EurekaClientApplication.class).web(true).run(args);
     }
 }
